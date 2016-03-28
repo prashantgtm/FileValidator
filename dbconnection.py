37105b09ad8084ab92a895683ff1bb70bf9b9326
prashant <prashantgtm@hotmail.com>
2016-03-28 16:35:29 +0545

    completed unit testing

--- dbconnection.py
+++ dbconnection.py
@@ -6,21 +6,19 @@ import sys
 class DbConnection:
     def __init__(self):
         configreader = ConfigReader()
-        config = configreader.readConfig()
-        host = config.get('database', 'host')
-        username = config.get('database', 'username')
-        password = config.get('database', 'password')
-        schema = config.get('database', 'schema')
-        try:
-            self.conn = MySQLdb.connect(host, username, password, schema)
-        except Exception as error:
-            sys.exit(error)
+        self.config = configreader.readConfig()
 
     def getDbConnection(self):
-        cursor = self.conn.cursor()
-        return cursor
+        host = self.config.get('database', 'host')
+        username = self.config.get('database', 'username')
+        password = self.config.get('database', 'password')
+        schema = self.config.get('database', 'schema')
 
-    def closeConnection(self):
-        self.conn.close()
+        try:
+            connection = MySQLdb.connect(host, username, password, schema)
+        except Exception as error:
+            sys.exit(error)
+        return connection
+
 
 
--- queries.py
+++ queries.py
@@ -4,12 +4,14 @@ import re
 
 class Queries:
     def __init__(self):
-        self.connection = DbConnection()
+        dbconnection = DbConnection()
         self.schema = 'shared_etl'
+        self.conn = dbconnection.getDbConnection()
 
     def getRawTableName(self, clientName, fileName):
-        cursor = self.connection.getDbConnection
-        query = "Select `reqexp` from %s.raw_control where client_name ='%s';" % (
+
+        cursor = self.conn.cursor()
+        query = "Select `regexp` from %s.raw_control where client_name ='%s';" % (
             self.schema, clientName)
         try:
             cursor.execute(query)
@@ -18,18 +20,18 @@ class Queries:
         regList = cursor.fetchall()
         for regExp in regList:
             if re.match(regExp[0], fileName):
-                query = "Select `table_name` from %s.raw_control where client_name = '%s' and `regexp` = '%s'" % (
+                query1 = "Select `table_name` from %s.raw_control where client_name = '%s' and `regexp` = '%s'" % (
                     self.schema, clientName, regExp[0])
         try:
-            cursor.execute(query)
+            cursor.execute(query1)
         except Exception as error:
             exit(error)
         tableName = cursor.fetchone()
-        self.connection.closeConnection()
-        return tableName
+        return tableName[0]
 
     def getDatabaseColumns(self, columnName, clientName, rawTable):
-        cursor = self.connection.getDbConnection
+        cursor = self.conn.cursor()
+
         query = "Select `%s` from %s.metadata_control where client_name = '%s' and raw_table_name = '%s' and raw_file_columns <> 'null';" % (
             columnName, self.schema, clientName, rawTable)
         try:
@@ -41,5 +43,8 @@ class Queries:
         for eachColumn in columns:
             eachColumn = [x.strip(',') for x in eachColumn]
             dbColumns.append(eachColumn[0])
-        self.connection.closeConnection()
+        cursor.close()
         return dbColumns
+
+
+
