cd1dfbcb0a0a4ffa3173de32b9d7d2cf1411eac5
prashant <prashantgtm@hotmail.com>
2016-03-28 10:51:46 +0545

    modeule to read the file

---
+++ readfile.py
@@ -0,0 +1,38 @@
+import csv
+from configreader import ConfigReader
+
+
+class ReadFile(object):
+    def __init__(self, file, clientName, fileType):
+        configreader = ConfigReader()
+        config = configreader.readConfig()
+        fileDelimiter = config.get(clientName, fileType + '.delimiter')
+        self.fileTopOffset = int(config.get(clientName, fileType + '.topOffset'))
+        self.fileLeftOffset = int(config.get(clientName, fileType + '.leftOffset'))
+
+        try:
+            with open(file, 'rb') as csvfile:
+                file = csv.reader(csvfile.read().decode('utf-8-sig').encode('utf-8').splitlines(),
+                                  delimiter=fileDelimiter, quotechar='"')
+                self.data = []
+                for line in file:
+                    self.data.append(line)
+        except Exception as error:
+            exit(error)
+
+    def getFileHeader(self):
+        header = self.data[self.fileTopOffset]
+        for i in range(self.fileLeftOffset):
+            header.pop(0)
+        return header
+
+    def getFileContent(self):
+        content = self.data[(self.fileTopOffset+1):]
+        for line in content:
+            for i in range(self.fileLeftOffset):
+                line.pop(0)
+        return content
+
+
+
+
